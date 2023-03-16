bebd286d571f50a8a40057f2c9b6faf87e4c07f7
ParticleG <particle_g@outlook.com>
2023-03-16 23:26:57 +0800

    - Update sources

--- src/routes/oicq/index.ts
+++ src/routes/oicq/index.ts
@@ -1,49 +1,25 @@
 import {FastifyPluginAsync} from "fastify";
 import {loginSchema} from "./schema";
 
-import {createClient} from "oicq";
-
-interface LoginBody {
-    account: number;
-    password: string;
-}
-
-enum LoginType {
-    Device,
-    QRCode,
-    Slider
-}
-
 const oicq: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     fastify.get('/', {websocket: true}, (connection, req) => {
         console.log(req);
         connection.socket.on('message', message => {
             console.log("Client: " + message.toString());
             connection.socket.send('hi from server')
-        })
+        });
     });
-    fastify.post<{ Body: LoginBody }>('/login', {schema: loginSchema}, (request, reply) => {
+    fastify.post<{
+        Body: { account: number; password: string; }
+    }>('/login', {schema: loginSchema}, async (request, reply) => {
         const {account, password} = request.body;
-        const client = createClient(account, {log_level: "warn"});
-        client.on("system.online", () => console.log("Logged in!"))
-        client.on("message", (e: any) => {
-            console.log(e)
-        })
-        client.on("system.login.device", (e: any) => {
-            console.log(e);
-            reply.send({type: LoginType.Device, data: e});
-            client.sendSmsCode();
-            process.stdin.once("data", code => client.submitSmsCode(String(code).trim()))
-        });
-        client.on("system.login.qrcode", (e: any) => {
-            reply.send({type: LoginType.QRCode, data: e});
-            process.stdin.once("data", () => client.login());
-        });
-        client.on("system.login.slider", (e: any) => {
-            reply.send({type: LoginType.Slider, data: e});
-            process.stdin.once("data", ticket => client.submitSlider(String(ticket).trim()))
-        });
-        client.login(password ? password : undefined);
+        return await fastify.login(account, password);
+    });
+    fastify.post<{
+        Body: { account: number; payload: string; }
+    }>('/postLogin', async (request, reply) => {
+        const {account, payload} = request.body;
+        return await fastify.postLogin(account, payload);
     });
 }
 
--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,6 +2,7 @@ import { FastifyPluginAsync } from 'fastify'
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
+    console.log(fastify.someSupport());
     return { root: true }
   })
 }
